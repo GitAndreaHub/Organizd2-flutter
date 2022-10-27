862ca5576ac34bffbbf78bc90170643bdb2942ed
GitAndreaHub <pipo2000a@libero.it>
2022-10-27 19:30:08 +0200

    Commit grafica timer.

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:organizd_2/main.dart';
 
 class Timer extends StatefulWidget {
   const Timer({Key? key}) : super(key: key);
@@ -10,16 +11,98 @@ class Timer extends StatefulWidget {
 class _TimerState extends State<Timer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-      (
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyTimerPage(),
+    );
+  }
+}
+
+class MyTimerPage extends StatefulWidget {
+  const MyTimerPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyTimerPage> createState() => _MyTimerPageState();
+}
+
+class _MyTimerPageState extends State<MyTimerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
       appBar: AppBar
         (
         title: const Text("Timer"),
       ),
-      body: const Center
-        (
-        child: Text("Timer page")
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Allineamento verticale della scritta
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Allineamento orizzontale della scritta
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "25:00",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 48,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 300,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RaisedButton(
+                onPressed: () {
+//                  _stopTimer();
+                },
+                color: Colors.white,
+                shape: CircleBorder(
+                  side: BorderSide(color: Colors.orange)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Text(
+                      "Stop",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+//                  _stopTimer();
+                },
+                color: Colors.orange,
+                shape: CircleBorder(
+                    side: BorderSide(color: Colors.orange)
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Text(
+                    "Start",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
+
     );
   }
 }
+
